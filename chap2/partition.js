0d2530123b3de645b3eab399be0f85ddb144d904
Sokol Sheri <sokolsheridev@gmail.com>
2021-08-12 19:18:24 -0400

    done

--- chap2/partition.js
+++ chap2/partition.js
@@ -1,15 +1,21 @@
 const { NodeDoubly, DoublyLinkedList } = require("./linkedlist");
 
-const node1 = new NodeDoubly(1);
-const node2 = new NodeDoubly(10);
-const node3 = new NodeDoubly(5);
-const node4 = new NodeDoubly(2);
+const node1 = new NodeDoubly(3);
+const node2 = new NodeDoubly(5);
+const node3 = new NodeDoubly(8);
+const node4 = new NodeDoubly(5);
+const node5 = new NodeDoubly(10);
+const node6 = new NodeDoubly(2);
+const node7 = new NodeDoubly(1);
 
 const newList = new DoublyLinkedList();
 newList.addToTail(node1);
 newList.addToTail(node2);
 newList.addToTail(node3);
 newList.addToTail(node4);
+newList.addToTail(node5);
+newList.addToTail(node6);
+newList.addToTail(node7);
 
 const partition = (x, ll) => {
   let node = ll.head;
@@ -30,4 +36,4 @@ const partition = (x, ll) => {
   return returnList;
 };
 
-partition(5, newList);
+console.log(partition(5, newList));

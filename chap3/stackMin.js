635a788b478ed94dff9c1101f7dbd6c2496a8cf5
Sokol Sheri <sokolsheridev@gmail.com>
2021-08-27 13:09:02 -0400

    done with stackMin

--- chap3/stackMin.js
+++ chap3/stackMin.js
@@ -1,12 +1,18 @@
 class Stack {
   constructor() {
     this.top = null;
+    this.min = null;
     this.currentLength = 0;
   }
 
   push(val) {
     this.currentLength++;
-    const node = new StackNode(val);
+    const node = new StackNode(val, this.min);
+    if (val < this.min) {
+      this.min = val;
+    } else if (this.min === null) {
+      this.min = val;
+    }
     let oldTop = this.top;
     this.top = node;
     this.top.next = oldTop;
@@ -19,6 +25,9 @@ class Stack {
     } else {
       const topNode = this.top;
       this.top = this.top.next;
+      if (topNode.value === this.min) {
+        this.min = topNode.min;
+      }
       --this.currentLength;
       return topNode.value;
     }
@@ -35,13 +44,26 @@ class Stack {
       return true;
     }
   }
-}
 
-class StackNode {
-  constructor(val) {
-    this.value = val;
-    this.next = null;
+  minimum() {
+    return this.min;
   }
 }
 
-module.exports = { Stack, StackNode };
+class StackNode {
+  constructor(val, min) {
+    this.value = val;
+    this.next = null;
+    this.min = min;
+  }
+}
+
+const stack = new Stack();
+stack.push(5);
+stack.push(6);
+stack.push(3);
+stack.push(7);
+stack.pop();
+stack.pop();
+
+console.log(stack.minimum());
